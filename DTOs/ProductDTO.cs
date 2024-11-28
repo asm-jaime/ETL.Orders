47f0b9155bf6598825c34766154c681dac7014c0
asm-jaime <asm.jaime@gmail.com>
2024-11-29 05:47:41 +1000

    add DTO for product purchase and user entities from xml

---
+++ DTOs/ProductDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ETL.Orders.DTOs;
+
+public class ProductDTO
+{
+    public string ProductName { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+}
---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ETL.Orders.DTOs;
+
+public class UserDTO
+{
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
