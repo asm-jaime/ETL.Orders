0f1f6aad883e3d3ceddefced587549eca5ecfcb4
asm-jaime <asm.jaime@gmail.com>
2024-11-29 18:04:36 +1000

    del unnecessary services

--- ETL.Orders.BLL/Services/ProductService.cs
+++
@@ -1,7 +0,0 @@
-﻿using ETL.Orders.DAL;
-namespace ETL.Orders.BLL.Services;
-
-public class ProductService(IProductRepository productRepository)
-{
-    IProductRepository _productRepository = productRepository;
-}
--- ETL.Orders.BLL/Services/UserService.cs
+++
@@ -1,8 +0,0 @@
-﻿using ETL.Orders.DAL;
-
-namespace ETL.Orders.BLL.Services;
-
-public class UserService(IUserRepository userRepository)
-{
-    IUserRepository _userRepository = userRepository;
-}
